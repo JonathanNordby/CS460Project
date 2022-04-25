5b1cbb7d81815d79d68a7ec21ea18c860b5cf84b
Jeff Rescignano <jeff@jeffresc.dev>
2022-04-24 22:36:46 -0400

    Fix proxy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'api/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/api', // You can pass in an array too eg. ['/api', '/another/path']
+        '/api',
         createProxyMiddleware({
             target: process.env.REACT_APP_PROXY_HOST,
             changeOrigin: true,
