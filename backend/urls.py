fc1ba97b01e289ad59deac9f1dd58f5dbd3e4ede
Jeff Rescignano <jeff@jeffresc.dev>
2022-04-24 22:01:23 -0400

    Create basic API

---
+++ backend/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from project import views
+
+router = routers.DefaultRouter()
+router.register(r'project', views.ProjectView, 'project')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProjectSerializer
+from .models import Project
+
 # Create your views here.
 
 def index(request):
 	return HttpResponse("Test Request: Please Ignore")
+
+class ProjectView(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
